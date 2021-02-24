2e5f24f5d6e2f546d8da65b9ce96ee474290b651
1203293671 <1203293671@qq.com>
2021-02-24 13:56:34 +0800

    newBranch

--- src/main/java/com/atguigu/git/Test.java
+++ src/main/java/com/atguigu/git/Test.java
@@ -4,7 +4,10 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("牛逼");
+        System.out.println("new");
+        System.out.println("new");
+        System.out.println("new");
+        System.out.println("new");
 
     }
 
